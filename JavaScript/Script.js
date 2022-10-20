67fa5fe558520044fe95dae022c19a30810c7b0f
DevMaik1 <110138747+DevMaik1@users.noreply.github.com>
2022-10-19 22:19:30 -0300

    Delete JavaScript directory

--- JavaScript/Script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('icon');
-
-function toggleMenu() {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-}
-
-btn.addEventListener('click', toggleMenu);
